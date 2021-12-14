8cfd4fb389258be5d77c1fe671875eceb2f0a690
David Lee <leedavid@microsoft.com>
2021-12-14 11:20:36 -0600

    deploy ingress + svc to AKS

--- Deployment/Deploy.ps1
+++ Deployment/Deploy.ps1
@@ -10,6 +10,45 @@ param(
     [string]$SqlPassword)
 
 $ErrorActionPreference = "Stop"
+# Prerequsites: 
+# * We have already assigned the managed identity with a role in Container Registry with AcrPull role.
+
+# Step 1: Deploy DB.
+Invoke-Sqlcmd -InputFile "$AppCode\Db\Migrations.sql" -ServerInstance $SqlServer -Database $DbName -Username $SqlUsername -Password $SqlPassword
+
+# Step 2: Login to AKS.
+az aks get-credentials --resource-group $AKS_RESOURCE_GROUP --name $AKS_NAME
+
+# Step 3: Create a namespace for your resources if it does not exist.
+$namespace = "myapps"
+$testNamespace = kubectl get namespace $namespace
+if (!$testNamespace ) {
+    kubectl create namespace $namespace
+}
+else {
+    Write-Host "Skip creating frontend namespace as it already exist."
+}
+
+# Step 4: Setup an external ingress controller
+$repoList = helm repo list --output json | ConvertFrom-Json
+$foundHelmIngressRepo = ($repoList | Where-Object { $_.name -eq "ingress-nginx" }).Count -eq 1
+
+# Add the ingress-nginx repository
+if (!$foundHelmIngressRepo ) {
+    helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+}
+else {
+    Write-Host "Skip adding ingress-nginx repo with helm as it already exist."
+}
+
+helm repo update
+helm install ingress-nginx ingress-nginx/ingress-nginx --create-namespace --namespace $namespace
+kubectl apply -f .\Deployment\external-ingress.yaml --namespace $namespace
+
+# Step 5: Setup configuration for resources
+$dbConnectionString = "Server=tcp:$SqlServer.database.windows.net,1433;Initial Catalog=appdb;Persist Security Info=False;User ID=$SqlUsername;Password=$SqlPassword;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=True;Connection Timeout=30;"
+# See: https://kubernetes.io/docs/concepts/configuration/secret/#use-case-dotfiles-in-a-secret-volume
+$base64DbConnectionString = [Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($dbConnectionString))
 
 $platformRes = (az resource list --tag stack-name=$NETWORKING_PREFIX | ConvertFrom-Json)
 if (!$platformRes) {
@@ -23,14 +62,13 @@ $acr = ($platformRes | Where-Object { $_.type -eq "Microsoft.ContainerRegistry/r
 if (!$acr) {
     throw "Unable to find eligible platform container registry!"
 }
-
 $acrName = $acr.Name
 
-Write-Host "ACR name: $acrName"
+# Step 6: Deploy customer service app.
+$content = Get-Content .\Deployment\customerservice.yaml
+$content = $content.Replace('$BASE64CONNECTIONSTRING', $base64DbConnectionString)
+$content = $content.Replace('$ACRNAME', $acrName)
+$content = $content.Replace('$NAMESPACE', $namespace)
 
-# Associate ACR with AKS
-az aks get-credentials --resource-group $AKS_RESOURCE_GROUP --name $AKS_NAME
-
-az aks update -n $AKS_NAME -g $AKS_RESOURCE_GROUP --attach-acr $acrName
-
-Invoke-Sqlcmd -InputFile "$AppCode\Db\Migrations.sql" -ServerInstance $SqlServer -Database $DbName -Username $SqlUsername -Password $SqlPassword
+Set-Content -Path ".\customerservice.yaml" -Value $content
+kubectl apply -f ".\customerservice.yaml" --namespace $namespace
