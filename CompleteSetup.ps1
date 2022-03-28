f6a455b0f3a52b4c9f6277ae4bede20d78741e68
David Lee <leedavid@microsoft.com>
2022-03-28 08:47:48 -0500

    update documentation

---
+++ CompleteSetup.ps1
@@ -0,0 +1,34 @@
+param($BUILD_ENV)
+
+$groups = az group list --tag stack-environment=$BUILD_ENV | ConvertFrom-Json
+$resourceGroupName = ($groups | Where-Object { $_.tags.'stack-name' -eq 'aks' -and $_.tags.'stack-environment' -eq $BUILD_ENV }).name
+$aks = (az resource list -g $resourceGroupName --resource-type "Microsoft.ContainerService/managedClusters" | ConvertFrom-Json)[0]
+az aks get-credentials -n $aks.name -g $resourceGroupName
+$acr = (az resource list --tag stack-name='shared-container-registry' | ConvertFrom-Json)[0]
+$acrName = $acr.name
+az aks update -n $aks.name -g $resourceGroupName --attach-acr $acrName
+if ($LastExitCode -ne 0) {
+    throw "An error has occured. Unable to update aks with acr."
+}
+
+$aksName = $aks.name
+$aksId = $aks.id
+$objectId = (az aks show -g $resourceGroupName -n $aksName --query addonProfiles.azureKeyvaultSecretsProvider.identity.objectId -o tsv)
+if ($LastExitCode -ne 0) {
+    throw "An error has occured. Unable to get object Id for performing role assignment."
+}
+
+az role assignment create --assignee $objectId --role "Key Vault Secrets User" --scope $aksId
+if ($LastExitCode -ne 0) {
+    throw "An error has occured. Unable to perform Key Vault Secrets User role asignment for aks."
+}
+
+az aks check-acr -n $aksName -g $resourceGroupName --acr "$acrName.azurecr.io"
+if ($LastExitCode -ne 0) {
+    throw "An error has occured. Unable to verify if aks and acr are connected."
+}
+
+kubectl get services -n myapps
+if ($LastExitCode -ne 0) {
+    throw "An error has occured. Unable to list all services"
+}
