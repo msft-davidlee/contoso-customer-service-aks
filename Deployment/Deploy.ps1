2b20b6355158ff0c5e31af0e53b9e5293ad766fb
David Lee <leedavid@microsoft.com>
2022-04-11 16:34:00 -0500

    configure load balancer

--- Deployment/Deploy.ps1
+++ Deployment/Deploy.ps1
@@ -170,6 +170,15 @@ helm install ingress-nginx ingress-nginx/ingress-nginx --namespace $namespace `
 
 helm install keda kedacore/keda -n $namespace
 
+$content = Get-Content .\Deployment\load-balancer-service.yaml
+$content = $content.Replace('$IP_ADD_RESOURCE_GROUP', $pipResGroup)
+$content = $content.Replace('$IP_ADD', $STATIC_IP)
+Set-Content -Path ".\load-balancer-service.yaml" -Value $content
+kubectl apply -f .\load-balancer-service.yaml --namespace $namespace
+if ($LastExitCode -ne 0) {
+    throw "An error has occured. Unable configure load balancer."
+}
+
 # if ($EnableFrontdoor) {
 #     $content = Get-Content .\Deployment\external-ingress-with-fd.yaml
 # }
