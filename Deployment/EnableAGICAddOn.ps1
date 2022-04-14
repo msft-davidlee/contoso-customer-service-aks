e86cda4db8fb52d5c75246fe2c4d749672ca09c0
David Lee <leedavid@microsoft.com>
2022-04-14 13:07:57 -0500

    cleanup

--- Deployment/EnableAGICAddOn.ps1
+++
@@ -1,9 +0,0 @@
-param(
-    [Parameter(Mandatory = $true)][string]$AKSName,
-    [Parameter(Mandatory = $true)][string]$AKResourceGroupSName,
-    [Parameter(Mandatory = $true)][string]$ApplicationGatewayResourceId)
-
-az aks enable-addons -n $AKSName -g $AKResourceGroupSName -a ingress-appgw --appgw-id $ApplicationGatewayResourceId
-if ($LastExitCode -ne 0) {
-    throw "An error has occured. Unable to enable add-ons for $AKSName and $ApplicationGatewayResourceId."
-}
