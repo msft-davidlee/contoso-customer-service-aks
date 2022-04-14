e8daccd4186bbfb99767c7120d249d757ea97361
David Lee <leedavid@microsoft.com>
2022-04-14 14:26:28 -0500

    hotfix for removing dev resources

--- Deployment/RemoveDevResources.ps1
+++ Deployment/RemoveDevResources.ps1
@@ -1,4 +1,4 @@
-[Parameter(Mandatory = $true)][string]$StackNameTag)
+param([Parameter(Mandatory = $true)][string]$StackNameTag)
 $stackRes = (az resource list --tag stack-name=$StackNameTag | ConvertFrom-Json)
 $devRes = $stackRes | Where-Object { $_.tags.'stack-environment' -eq 'dev' }
 if ($devRes -and $devRes.Length -gt 0) {
