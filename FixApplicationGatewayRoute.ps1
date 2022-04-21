3998745a22cd0203e76c8aad0574290f86248c52
David Lee <leedavid@microsoft.com>
2022-04-21 12:00:51 -0500

    temp workaround for incorrect routes

---
+++ FixApplicationGatewayRoute.ps1
@@ -0,0 +1,16 @@
+# The routing rules configured in Application Gateway are incorrect. This is a temp workaround to
+# address this issue for a working demo.
+
+param([Parameter(Mandatory = $true)][string]$BUILD_ENV)
+
+$groups = az group list --tag stack-environment=$BUILD_ENV | ConvertFrom-Json
+$resourceGroupName = ($groups | Where-Object { $_.tags.'stack-name' -eq 'aks' -and $_.tags.'stack-environment' -eq $BUILD_ENV }).name
+
+$appGw = (az resource list -g $resourceGroupName --resource-type "Microsoft.Network/applicationGateways" | ConvertFrom-Json)[0]
+if ($appGw) {
+
+    $rules = az network application-gateway rule list --gateway-name $appGw.name --g $appGw.resourceGroup | ConvertFrom-Json
+    $rules | ForEach-Object {
+        $_.name
+    }
+}
