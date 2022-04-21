7ae4b34453c9ac5b245d7cca67c00f6e85f282e9
David Lee <leedavid@microsoft.com>
2022-04-21 12:34:43 -0500

    update agic

--- Deployment/ResolveSharedResources.ps1
+++ Deployment/ResolveSharedResources.ps1
@@ -85,6 +85,14 @@ $EnableApplicationGateway = (az appconfig kv show -n $configName --key "$StackNa
 if ($LastExitCode -ne 0) {
     throw "An error has occured. Unable to get enable-application gateway flag  from $configName."
 }
+# Does an existing app gw already exist? If it does, don't do a deployment as it might
+# cause the settings to be reset as settings are managed via AGIC.
+if ($EnableApplicationGateway -eq "true") {
+    $exist = (az resource list -g $appResourceGroup --resource-type "Microsoft.Network/applicationGateways" | ConvertFrom-Json).Length
+    if ($exist -eq 1) {
+        $EnableApplicationGateway = "false"
+    }
+}
 Write-Host "::set-output name=enableApplicationGateway::$EnableApplicationGateway"
 
 $certDomainNamesJson = (az appconfig kv show -n $configName --key "$StackNameTag/cert-domain-names" --auth-mode login | ConvertFrom-Json).value
