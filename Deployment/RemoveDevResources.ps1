a13493802cc219ad689ab7375e0613a4e770a023
David Lee <leedavid@microsoft.com>
2022-04-21 17:57:53 -0500

    hotfix only remove items from dev

--- Deployment/RemoveDevResources.ps1
+++ Deployment/RemoveDevResources.ps1
@@ -1,14 +1,18 @@
 param(
-    [Parameter(Mandatory = $true)][string]$StackNameTag, 
-    [Parameter(Mandatory = $true)][string]$BUILD_ENV)
+    [Parameter(Mandatory = $true)][string]$StackNameTag)
 
+$BUILD_ENV = "dev"
 $groups = az group list --tag stack-environment=$BUILD_ENV | ConvertFrom-Json
 $resourceGroupName = ($groups | Where-Object { $_.tags.'stack-name' -eq 'aks' -and $_.tags.'stack-environment' -eq $BUILD_ENV }).name
 
+$count = 0
 $stackRes = (az resource list --tag stack-name=$StackNameTag | ConvertFrom-Json)
 $devRes = $stackRes | Where-Object { $_.tags.'stack-environment' -eq 'dev' }
 if ($devRes -and $devRes.Length -gt 0) {
     if ($devRes.resourceGroup -eq $resourceGroupName) {
         az resource delete --id $devRes.id
+        $count +=1
     }    
 }
+
+Write-Host "Number of resource deleted: $count"
